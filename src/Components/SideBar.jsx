2f8d9b65751aa1f96ecb50605a26d4e9d9d0ac3d
Priyankar_010_ <109033813+PriyankarCode010@users.noreply.github.com>
2024-08-20 09:35:52 +0530

    final

--- src/Components/SideBar.jsx
+++ src/Components/SideBar.jsx
@@ -46,9 +46,9 @@ const SideBar = ({menuItem,session}) => {
                     ))}
                     <div className="absolute inset-x-0 bottom-0 border-t flex items-center justify-center text-white dark:text-white">
                         <div className={`flex justify-between items-center overflow-hidden transition-all ${expanded ? "w-52 p-4" : "w-0 "}`}>
-                        <span className={`${expanded?"p-1":"px-4 py-1"}`}>
-                            <Image src="/user.png" alt="logo"/>
-                        </span>
+                        <div className={`${expanded?"p-1":"px-4 py-1"}`}>
+                            <Image src="/user.png" width={50} height={50} alt="User Image" />
+                        </div>
                             <div>
                                 <div className="leading-4">
                                     <h4 className="font-semibold">{session?.user?.name}</h4>
